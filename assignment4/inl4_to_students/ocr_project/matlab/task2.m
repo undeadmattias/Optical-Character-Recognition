252006992370054cb46f6a7b77d36634b63b5e2a
elt13ms1 <elt13ms1@pax-18.net.lth.se>
2017-10-11 11:46:03 +0200

    Task 2a / 2.1) complete, assignment 4 .
    Gaussian distrubitions estimated.

--- assignment4/inl4_to_students/ocr_project/matlab/task2.m
+++ assignment4/inl4_to_students/ocr_project/matlab/task2.m
@@ -1,4 +1,21 @@
 % Graph cuts
 load heart_data
+hold on
 
+mu = mean(background_values);
+sigma = std(background_values);
 
+x = (-5 * sigma:0.01:5 * sigma) + mu;  %// Plotting range
+y = exp(- 0.5 * ((x - mu) / sigma) .^ 2) / (sigma * sqrt(2 * pi));
+plot(x, y, 'b')
+
+mu = mean(chamber_values);
+sigma = std(chamber_values);
+
+x = (-5 * sigma:0.01:5 * sigma) + mu;  %// Plotting range
+y = exp(- 0.5 * ((x - mu) / sigma) .^ 2) / (sigma * sqrt(2 * pi));
+
+plot(x, y, 'r')
+
+% Plotting the gaussian distributions
+% The blue is the background and the red is the chamber class
